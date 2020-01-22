0525943bf0d7825b3bcdb314ba494ecd596324f5
Alehandro Persaud <andrew041994@gmail.com>
2020-01-22 20:50:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
       array[-2] << " and "
       return array.join
     else array.size == 3
-      array[-3] << " and "
+      array[-2] << " and "
       array.join(", ")
       #array.join (" , ")
 
