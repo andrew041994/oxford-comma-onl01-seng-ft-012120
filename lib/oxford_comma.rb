dbbb7151b36ada6a4a2f4ce6bf2638dfeb0b24e8
Alehandro Persaud <andrew041994@gmail.com>
2020-01-22 21:28:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     elsif array.length == 1
     	array.join
     else array.length >=3
-      array[-2] << " and "
+      array[-2] = " and "
   	return array * ", "
     end
 end
