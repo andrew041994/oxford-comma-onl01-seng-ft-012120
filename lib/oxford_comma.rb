a0545ef3d44d993a0f12320d9e1d679ed6d3847c
Alehandro Persaud <andrew041994@gmail.com>
2020-01-22 20:23:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 
     if array.size < 2
       return array.join
-    elsif array.size > 1
+    else array.size > 1
 
       array[-2] << " and "
       return array.join(",")
