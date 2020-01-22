618a4f2f3b42eb1f862c82350807e175818df073
Alehandro Persaud <andrew041994@gmail.com>
2020-01-22 20:49:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,8 @@ def oxford_comma(array)
       array[-2] << " and "
       return array.join
     else array.size == 3
-      array[-2] << " and "
-      array.split
+      array[-3] << " and "
+      array.join(" , ")
       #array.join (" , ")
 
 
