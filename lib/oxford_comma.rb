41aaffd2e354992a468f696f3bf3c1e6aa3512e3
Alehandro Persaud <andrew041994@gmail.com>
2020-01-22 21:31:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     elsif array.length == 1
     	array.join
     else array.length >=3
-      array[-1] = "and #{array[-1]} "
+      array[-1] = "and #{array[-1]}"
   	return array * ", "
     end
 end
