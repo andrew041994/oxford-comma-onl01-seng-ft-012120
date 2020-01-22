265c50abb2d822cd5f7096902c11b684534ed47b
Alehandro Persaud <andrew041994@gmail.com>
2020-01-22 20:49:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
 def oxford_comma(array)
 
-    if array.size < 2
+    if array.size == 1
       return array.join
-    elsif array.size > 1
+    elsif array.size == 2
       array[-2] << " and "
       return array.join
     else array.size == 3
       array[-2] << " and "
       array.split
-      array.join (" , ")
+      #array.join (" , ")
 
 
     end
